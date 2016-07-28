359b3b3c772adc5adae6aed6900d741fbc503bcb
Angel Garcia Reyes <azmavet@gmail.com>
2016-07-28 13:00:15 -0700

    Simpler solution to IsBinaryTreeMirrored.

--- csharp-algorithms/csharp-algorithms/Challenges.cs
+++ csharp-algorithms/csharp-algorithms/Challenges.cs
@@ -24,55 +24,22 @@ namespace csharp_algorithms
                 return false;
             }
 
-            List<Node> children = new List<Node>();
-            children.Add(root.Left);
-            children.Add(root.Right);
-
-            return IsbinTreeMirrored(children);
+            return IsbinTreeMirrored(root.Left, root.Right);
         }
 
-        private static bool IsbinTreeMirrored(List<Node> children)
+        private static bool IsbinTreeMirrored(Node left, Node right)
         {
-            if (children.Count == 0)
+            if (left == null && right == null)
             {
                 return true;
             }
 
-            for (int i = 0; i < children.Count / 2; i++)
+            if (left == null || right == null || left.Data != right.Data)
             {
-                var indexMirror = children.Count - 1 - i;
-                if (children[i] == null && children[indexMirror] != null)
-                {
-                    return false;
-                }
-
-                if (children[i] != null && children[indexMirror] == null)
-                {
-                    return false;
-                }
-
-                if (children[i] == null && children[indexMirror] == null)
-                {
-                    continue;
-                }
-
-                if (children[i].Data != children[children.Count - 1 - i].Data)
-                {
-                    return false;
-                }
+                return false;
             }
 
-            List<Node> grandChildren = new List<Node>();
-            foreach (Node child in children)
-            {
-                if (child != null)
-                {
-                    grandChildren.Add(child.Left);
-                    grandChildren.Add(child.Right);
-                }
-            }
-
-            return IsbinTreeMirrored(grandChildren);
+            return IsbinTreeMirrored(left.Left, right.Right) && IsbinTreeMirrored(left.Right, right.Left);
         }
     }
 }
