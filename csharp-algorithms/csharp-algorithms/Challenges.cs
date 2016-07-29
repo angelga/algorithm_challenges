7b088c4c8aec60cc82760152afd7f65581292f50
Angel Garcia Reyes <azmavet@gmail.com>
2016-07-28 18:46:34 -0700

    Tree Traversal problems.

--- csharp-algorithms/csharp-algorithms/Challenges.cs
+++ csharp-algorithms/csharp-algorithms/Challenges.cs
@@ -27,6 +27,13 @@ namespace csharp_algorithms
             return IsbinTreeMirrored(root.Left, root.Right);
         }
 
+        /// <summary>
+        /// Given two nodes, ensure they are ether both null, or both have same Data.
+        /// If so continue checking outer children and inner children recursively.
+        /// </summary>
+        /// <param name="left"></param>
+        /// <param name="right"></param>
+        /// <returns></returns>
         private static bool IsbinTreeMirrored(Node left, Node right)
         {
             if (left == null && right == null)
@@ -40,6 +47,74 @@ namespace csharp_algorithms
             }
 
             return IsbinTreeMirrored(left.Left, right.Right) && IsbinTreeMirrored(left.Right, right.Left);
+        }
+
+        /// <summary>
+        /// Get the depth (levels) of a binary tree. It's assumed that:
+        /// 1. Null root = depth of zero
+        /// 2. Root only (no children) = depth of 1
+        /// </summary>
+        /// <param name="root"></param>
+        /// <returns></returns>
+        public static int DepthBinaryTree(Node root)
+        {
+            if (root == null)
+            {
+                return 0;
+            }
+
+            return 1 + Math.Max(DepthBinaryTree(root.Left), DepthBinaryTree(root.Right));
+        }
+
+        /// <summary>
+        /// Given a binary tree, traverse the nodes in horizontal, left to right mode.
+        /// For instance:
+        ///     8
+        ///   1   2
+        ///  2 3 4 5
+        ///  
+        ///  Output: 8 1 2 2 3 4 5
+        /// </summary>
+        /// <param name="root"></param>
+        /// <returns></returns>
+        public static string TraverseTreeHorizontally(Node root)
+        {
+            if (root == null)
+            {
+                return string.Empty;
+            }
+
+            Queue<Node> nodes = new Queue<Node>();
+            nodes.Enqueue(root);
+
+            return TraverseTreeHor(nodes);
+        }
+
+        /// <summary>
+        /// Use Queue data structure to visit nodes horizontally in a binary tree.
+        /// </summary>
+        /// <param name="nodes"></param>
+        /// <returns></returns>
+        private static string TraverseTreeHor(Queue<Node> nodes)
+        {
+            List<int> result = new List<int>();
+
+            while (nodes.Count > 0)
+            {
+                Node last = nodes.Dequeue();
+                result.Add(last.Data);
+                if (last.Left != null)
+                {
+                    nodes.Enqueue(last.Left);
+                }
+
+                if (last.Right != null)
+                {
+                    nodes.Enqueue(last.Right);
+                }
+            }
+
+            return string.Join(", ", result);
         }
     }
 }
--- csharp-algorithms/csharp-algorithmsTests/ChallengesTests.cs
+++ csharp-algorithms/csharp-algorithmsTests/ChallengesTests.cs
@@ -103,5 +103,103 @@ namespace csharp_algorithms.Tests
 
             Assert.IsFalse(Challenges.IsBinaryTreeMirrored(root));
         }
+
+        [TestMethod()]
+        public void DepthBinTree_Null()
+        {
+            Assert.AreEqual(0, Challenges.DepthBinaryTree(null));
+        }
+
+        [TestMethod()]
+        public void DepthBinTree_OneLevel()
+        {
+            Assert.AreEqual(1, Challenges.DepthBinaryTree(new Node(5)));
+        }
+
+        [TestMethod()]
+        public void DepthBinTree_TwoLevelIncomplete()
+        {
+            Node root = new Node(5);
+            root.Left = new Node(4);
+
+            Assert.AreEqual(2, Challenges.DepthBinaryTree(root));
+        }
+
+        [TestMethod()]
+        public void DepthBinTree_TwoLevelComplete()
+        {
+            Node root = new Node(5);
+            root.Left = new Node(4);
+            root.Right = new Node(6);
+
+            Assert.AreEqual(2, Challenges.DepthBinaryTree(root));
+        }
+
+        [TestMethod()]
+        public void DepthBinTree_ThreeLevelIncomplete()
+        {
+            Node root = new Node(5);
+            root.Left = new Node(4);
+            root.Right = new Node(6);
+            root.Right.Left = new Node(10);
+
+            Assert.AreEqual(3, Challenges.DepthBinaryTree(root));
+        }
+
+        [TestMethod()]
+        public void DepthBinTree_ThreeLevelComplete()
+        {
+            Node root = new Node(5);
+            root.Left = new Node(4);
+            root.Right = new Node(6);
+            root.Left.Left = new Node(1);
+            root.Left.Right = new Node(2);
+            root.Right.Left = new Node(8);
+            root.Right.Right = new Node(9);
+
+            Assert.AreEqual(3, Challenges.DepthBinaryTree(root));
+        }
+
+        [TestMethod()]
+        public void DepthBinTree_FourLevelIncomplete()
+        {
+            Node root = new Node(5);
+            root.Right = new Node(6);
+            root.Right.Left = new Node(8);
+            root.Right.Left.Right = new Node(9);
+
+            Assert.AreEqual(4, Challenges.DepthBinaryTree(root));
+        }
+
+        [TestMethod()]
+        public void TraverseTreeHorizontally_Empty()
+        {
+            Assert.AreEqual(string.Empty, Challenges.TraverseTreeHorizontally(null));
+        }
+
+        [TestMethod()]
+        public void TraverseTreeHorizontally_RootOnly()
+        {
+            Assert.AreEqual("1", Challenges.TraverseTreeHorizontally(new Node(1)));
+        }
+
+        [TestMethod()]
+        public void TraverseTreeHorizontally_OneLevel()
+        {
+            Node root = new Node(1);
+            root.Right = new Node(2);
+            Assert.AreEqual("1, 2", Challenges.TraverseTreeHorizontally(root));
+        }
+
+        [TestMethod()]
+        public void TraverseTreeHorizontaly_ThreeLevels()
+        {
+            Node root = new Node(5);
+            root.Left = new Node(4);
+            root.Left.Right = new Node(0);
+            root.Right = new Node(8);
+
+            Assert.AreEqual("5, 4, 8, 0", Challenges.TraverseTreeHorizontally(root));
+        }
     }
 }
